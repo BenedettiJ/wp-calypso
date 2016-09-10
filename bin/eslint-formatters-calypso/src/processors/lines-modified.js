b3c398675801a72af236ed5f87770d4bdf29dcf2
nssw <nssw@users.noreply.github.com>
2016-09-10 10:33:01 +0200

    eslines becomes a npm package

--- bin/eslint-formatters-calypso/src/processors/lines-modified.js
+++
@@ -1,8 +0,0 @@
-const eslines = require( '../lib/eslines' );
-const differ = require( '../lib/differ' );
-const gitDiffCalculator = require( '../lib/git-diff-calculator' );
-
-module.exports = function( report ) {
-	const lines = differ( gitDiffCalculator() );
-	return JSON.stringify( eslines( report, lines ) );
-};
